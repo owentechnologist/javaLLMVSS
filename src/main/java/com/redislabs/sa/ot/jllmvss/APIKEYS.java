9e13b839482bb9ffef3d5188a7509fdf459970a1
Owen Taylor <48262631+owentechnologist@users.noreply.github.com>
2023-10-23 01:06:27 -0500

    removed Hard-Coded Keys for OpenAI service

--- src/main/java/com/redislabs/sa/ot/jllmvss/APIKEYS.java
+++ src/main/java/com/redislabs/sa/ot/jllmvss/APIKEYS.java
@@ -1,7 +1,7 @@
 package com.redislabs.sa.ot.jllmvss;
 
 public class APIKEYS {
-    public final static String OPENAI_GPT_KEY="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";
-    public final static String OPENAI_DEMO_KEY="demo";
+    public final static String OPENAI_GPT_KEY=System.getenv("OPENAI_GPT_KEY");
+    public final static String OPENAI_DEMO_KEY=System.getenv("demo_key");
 
 }
